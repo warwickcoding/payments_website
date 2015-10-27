cac7472911958ccf3723a2697cad16728172ce0a
Zaid Al Lahham <zlahham@gmail.com>
2015-10-27 15:28:01 +0000

    Seperated the course payments in seperate views

--- app.rb
+++ app.rb
@@ -10,6 +10,18 @@ get '/' do
   erb :index
 end
 
+get '/payment_frontend' do
+  erb :payment_frontend
+end
+
+get '/payment_android_early' do
+  erb :payment_android_early
+end
+
+get '/payment_android_regular' do
+  erb :payment_android_regular
+end
+
 post '/chargefrontend' do
   # Amount in cents
   @amount = 7000
@@ -48,6 +60,25 @@ post '/chargeandroid' do
   erb :chargeandroid
 end
 
+post '/chargeandroidearly' do
+  # Amount in cents
+  @amount = 8000
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :card  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Android Early Charge',
+    :currency    => 'gbp',
+    :customer    => customer.id
+  )
+
+  erb :chargeandroidearly
+end
+
 __END__
 
 @@ layout
@@ -74,53 +105,109 @@ __END__
     </div>
 
     <div class="row">
-      <div class="col-md-2 col-md-offset-4">
-
-        <form action="/chargefrontend" method="post" class="payment">
-          <article>
-            <label class="amount">
-              <span>Front-end: £70.00</span>
-            </label>
-          </article>
-
-          <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                  data-key="<%= settings.publishable_key %>"
-                  data-description="Frontend course"
-                  data-amount="7000"
-                  data-currency="gbp"
-                  data-locale="auto"></script>
-        </form>
-
-      </div>
-
-      <div class="col-md-2">
-        <form action="/chargeandroid" method="post" class="payment">
-          <article>
-            <label class="amount">
-              <span>Android: £90.00</span>
-            </label>
-          </article>
-
-          <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                  data-key="<%= settings.publishable_key %>"
-                  data-description="Android course"
-                  data-amount="9000"
-                  data-currency="gbp"
-                  data-locale="auto"></script>
-        </form>
+      <div class="col-md-6 col-md-offset-3">
+        <ul>
+          <li><a href="/payment_frontend">Front-End Course Payment</a></li>
+          <li><a href="/payment_android_early">Android Early Bird Course Payment</a></li>
+          <li><a href="/payment_android_regular">Android Course Payment</a></li>
+        </ul>
       </div>
     </div>
-
   </div>
 
+@@ payment_android_early
+  <div class="row">
+    <div class="col-md-2 col-md-offset-5">
+      <form action="/chargeandroidearly" method="post" class="payment">
+        <article>
+          <label class="amount">
+            <span>Android Early Bird: £80.00</span>
+          </label>
+        </article>
 
+        <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                data-key="<%= settings.publishable_key %>"
+                data-description="Android course"
+                data-amount="8000"
+                data-currency="gbp"
+                data-locale="auto"></script>
+      </form>
+    </div>
+  </div>
+
+@@ payment_android_regular
+  <div class="row">
+    <div class="col-md-2 col-md-offset-5">
+      <form action="/chargeandroid" method="post" class="payment">
+        <article>
+          <label class="amount">
+            <span>Android: £90.00</span>
+          </label>
+        </article>
+
+        <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                data-key="<%= settings.publishable_key %>"
+                data-description="Android course"
+                data-amount="9000"
+                data-currency="gbp"
+                data-locale="auto"></script>
+      </form>
+    </div>
+  </div>
+
+@@ payment_frontend
+  <div class="row">
+    <div class="col-md-2 col-md-offset-5">
+
+      <form action="/chargefrontend" method="post" class="payment">
+        <article>
+          <label class="amount">
+            <span>Front-end: £70.00</span>
+          </label>
+        </article>
+
+        <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                data-key="<%= settings.publishable_key %>"
+                data-description="Frontend course"
+                data-amount="7000"
+                data-currency="gbp"
+                data-locale="auto"></script>
+      </form>
+    </div>
+  </div>
+
+@@ chargeandroidearly
+  <div class="container">
+    <div class='row'>
+      <div class="col-md-6 col-md-offset-3">
+        <img src="logo_two_layers.png" alt="Warwick coding logo" />
+     </div>
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+      <h2>Thanks, you paid <strong>£80.00</strong>!</h2>
+    </div>
+  </div>
 
 @@ chargeandroid
-  <div class="col-md-6 col-md-offset-3">
-    <h2>Thanks, you paid <strong>£90.00</strong>!</h2>
+  <div class="container">
+    <div class='row'>
+      <div class="col-md-6 col-md-offset-3">
+        <img src="logo_two_layers.png" alt="Warwick coding logo" />
+     </div>
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+      <h2>Thanks, you paid <strong>£90.00</strong>!</h2>
+    </div>
   </div>
 
 @@ chargefrontend
-  <div class="col-md-6 col-md-offset-3">
-    <h2>Thanks, you paid <strong>£70.00</strong>!</h2>
+  <div class="container">
+    <div class='row'>
+      <div class="col-md-6 col-md-offset-3">
+        <img src="logo_two_layers.png" alt="Warwick coding logo" />
+     </div>
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+      <h2>Thanks, you paid <strong>£70.00</strong>!</h2>
+    </div>
   </div>
